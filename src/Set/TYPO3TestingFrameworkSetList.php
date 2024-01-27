464375850b5620c8dbdd6cdc410bb77a9fa4190f
Sebastian Schreiber <me@schreibersebastian.de>
2024-01-27 14:18:03 +0100

    TASK: Add config for testing framework version 7

---
+++ src/Set/TYPO3TestingFrameworkSetList.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ssch\Typo3RectorTestingFramework\Set;
+
+final class TYPO3TestingFrameworkSetList
+{
+    public const TYPO3_TESTING_FRAMEWORK_7 = __DIR__ . '/../../config/sets/typo3-testingframework/typo3-testingframework-7.php';
+}
