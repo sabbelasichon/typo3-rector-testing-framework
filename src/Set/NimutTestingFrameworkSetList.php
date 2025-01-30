3c986a2240d0901acc8e7c5a02ba2bf988b76055
Sebastian Schreiber <me@schreibersebastian.de>
2025-01-30 09:05:23 +0100

    TASK: Prepare release for version 3.x

--- src/Set/NimutTestingFrameworkSetList.php
+++ src/Set/NimutTestingFrameworkSetList.php
@@ -6,7 +6,7 @@ namespace Ssch\Typo3RectorTestingFramework\Set;
 
 use Rector\Set\Contract\SetListInterface;
 
-final class NimutTestingFrameworkSetList implements SetListInterface
+final class NimutTestingFrameworkSetList
 {
     public const NIMUT_TESTING_FRAMEWORK_TO_TYPO3_TESTING_FRAMEWORK = __DIR__ . '/../../config/sets/nimut-testingframework/nimut-testing-framework-to-typo3-testing-framework.php';
 }
